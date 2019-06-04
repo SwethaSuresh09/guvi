f75aa620fb5c0dc2a6e2408a7f5a3a3c0f8d1414
SwethaSuresh09 <51352554+SwethaSuresh09@users.noreply.github.com>
2019-06-04 11:24:28 +0400

    Create qntwo.py

---
+++ codes/qntwo.py
@@ -0,0 +1,5 @@
+chr=input()
+if(chr=='a' or chr=='A' or chr=='e' or chr=='E' or chr=='i' or chr=='I' or chr=='o' or chr=='O' or chr=='u' or chr=='U'):
+    print("Vowel")
+else:
+    print("Consonant")
