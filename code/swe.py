7a336f653392188777f9c5c68b093b05d52a4196
SwethaSuresh09 <51352554+SwethaSuresh09@users.noreply.github.com>
2019-07-26 22:22:21 +0530

    Update swe.py

--- code/swe.py
+++ code/swe.py
@@ -1,7 +1,7 @@
 z11=int(input())
 z22=list(map(int,input().split()))
 z33=int(len(z22)/2)
-if sum(z22[:z33])//len(z22[:z33])==sum(z22[z33:])//len(len(z22[z33:]):
+if sum(z22[:z33])//len(z22[:z33])==sum(z22[z33:])//len(z22[z33:]):
     print('yes')
 else:
     print('no')
