7e67612f239387aedb84793a49f2cd81d6838b36
SwethaSuresh09 <51352554+SwethaSuresh09@users.noreply.github.com>
2019-07-26 21:43:49 +0530

    Update pro12.py

--- code/pro12.py
+++ code/pro12.py
@@ -1,12 +1,12 @@
 kk=int(input())
 qq=[]
 for j in range(0,kk):
-rr=input()
-qq.append(rr)
+  rr=input()
+  qq.append(rr)
 ss=[]
 for k in zip(*qq):
-if(j.count(j[0]==len(j)):
-ss.append(j[0])
-else:
-break
+  if(j.count(j[0]==len(j)):
+    ss.append(j[0])
+  else:
+    break
 print(''.join(ss))
